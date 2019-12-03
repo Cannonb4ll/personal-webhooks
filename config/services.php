93280fc93a67e903d6835f6fb53ab301c3d4fb49
Dennis <dennissmink@gmail.com>
2019-12-03 14:32:57 +0100

    Added philips hue

--- config/services.php
+++ config/services.php
@@ -30,4 +30,12 @@ return [
         'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
     ],
 
+    'philips-hue' => [
+        'client_id' => env('PHILIPS_HUE_CLIENT_ID'),
+        'client_secret' => env('PHILIPS_HUE_CLIENT_SECRET'),
+        'app_id' => env('PHILIPS_HUE_APP_ID'),
+        'device_id' => env('PHILIPS_HUE_DEVICE_ID'),
+        'user' => env('PHILIPS_HUE_USERNAME')
+    ]
+
 ];
